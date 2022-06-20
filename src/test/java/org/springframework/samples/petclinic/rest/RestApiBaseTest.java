7b49ef84ae5b0e590ffd2631cd9d9060eb522530
s.saparbekov <s.saparbekov@tinkoff.ru>
2022-06-20 20:23:48 +0300

    feat: add tests

--- src/test/java/org/springframework/samples/petclinic/rest/RestApiBaseTest.java
+++ src/test/java/org/springframework/samples/petclinic/rest/RestApiBaseTest.java
@@ -1,5 +1,7 @@
 package org.springframework.samples.petclinic.rest;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -9,14 +11,22 @@ import java.sql.SQLException;
 
 public class RestApiBaseTest {
 
-	private static String DB_URL = "jdbc:postgresql://localhost:5432/petclinic";
-	private static String DB_USER = "petclinic";
-	private static String DB_PASSWORD = "petclinic";
+	static final String BASE_URI = "http://localhost";
+	static final int PORT = 8080;
+	private static final String DB_URL = "jdbc:postgresql://localhost:5432/petclinic";
+	private static final String DB_USER = "petclinic";
+	private static final String DB_PASSWORD = "petclinic";
 
+	static RequestSpecification requestSpec;
 	public static Connection connection;
 
 	@BeforeAll
 	static void beforeAll() throws SQLException {
+		requestSpec = new RequestSpecBuilder().
+			setBaseUri(BASE_URI).
+			setPort(PORT).
+			build();
+
 		connection = DriverManager.getConnection(
 			DB_URL, DB_USER, DB_PASSWORD
 		);
